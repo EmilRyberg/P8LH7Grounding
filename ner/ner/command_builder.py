02fe5386558a78c4e359ebbf4f30acff051fd350
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-03-01 10:54:00 +0100

    Added support for FindTask and child tasks

--- ner/ner/command_builder.py
+++ ner/ner/command_builder.py
@@ -43,19 +43,29 @@ class ObjectEntity:
                 spatial_type = SpatialType(word.lower())
                 self.spatial_descriptor = SpatialDescription(spatial_type, entities[index+1:])
                 break
+            elif entity_type == EntityType.TAKE or entity_type == EntityType.FIND:
+                break
         self.build_name()
         return self
 
     def __str__(self):
         output = f"[{self.name}]"
-        if self.spatial_descriptor:
+        if self.spatial_descriptor is not None:
             output += f" --> {self.spatial_descriptor}"
         return output
 
 
 class BaseTask:
     def __init__(self):
-        self.child_tasks = None
+        self.child_tasks = []
+
+    def __str__(self):
+        if len(self.child_tasks) == 0:
+            return ""
+        output_str = f"Child tasks:\n"
+        for task in self.child_tasks:
+            output_str += f"\t{task}"
+        return output_str
 
 
 class PickUpTask(BaseTask):
@@ -68,13 +78,20 @@ class PickUpTask(BaseTask):
         return self
 
     def __str__(self):
-        return f"Task type: {PickUpTask.__name__}\n\tObject to pick up: {self.object_to_pick_up}"
+        return f"Task type: {PickUpTask.__name__}\n\tObject to pick up: {self.object_to_pick_up}\n{super().__str__()}"
 
 
 class FindTask(BaseTask):
     def __init__(self):
         super().__init__()
-        self.object_to_find = None
+        self.object_to_find = ObjectEntity()
+
+    def build_task(self, entities):
+        self.object_to_find.build_object(entities)
+        return self
+
+    def __str__(self):
+        return f"Task type: {FindTask.__name__}\n\tObject to find: {self.object_to_find}\n{super().__str__()}"
 
 
 class CommandBuilder:
@@ -83,22 +100,20 @@ class CommandBuilder:
 
     def get_task(self, sentence):
         entities = self.ner.get_entities(sentence)
-        #print(entities)
         task = None
         task_type = None
         for index, (entity_type, word) in enumerate(entities):
             if entity_type == EntityType.TAKE:
                 if task_type is not None:
-                    print("Found another task, which is not allowed")  # for now
-                    raise NotImplementedError()
+                    task.child_tasks.append(PickUpTask().build_task(entities[index+1:]))
                 else:
                     task_type = EntityType.TAKE
                     task = PickUpTask().build_task(entities[index+1:])
             elif entity_type == EntityType.FIND:
                 if task_type is not None:
-                    print("Found another task, which is not allowed")  # for now
-                    raise NotImplementedError()
+                    task.child_tasks.append(FindTask().build_task(entities[index+1:]))
                 else:
                     task_type = EntityType.FIND
-                    task = FindTask()
+                    task = FindTask().build_task(entities[index+1:])
         return task
+
