ee6e0a6269630a00bfbde248df0b430823936eea
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-03-02 16:09:45 +0100

    Basic service node for NER

--- ner/nodes/ner_node.py
+++ ner/nodes/ner_node.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
-from ner_stuff.ner_stuff import ner_test
+from ner.command_builder import CommandBuilder
+from ner.ner import EntityType
+from little_helper_interfaces.msg import Task, ObjectEntity, OuterObjectEntity, SpatialDescription
+from ner.srv import NER, NERResponse, NERRequest
 
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
-    ner_test()
+class DummyNER:
+    def get_entities(self, sentence):
+        return [
+            (EntityType.FIND, "find"),
+            (EntityType.COLOUR, "blue"),
+            (EntityType.OBJECT, "bottom cover")
+        ]
 
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+
+class NERService:
+    def __init__(self):
+        self.cmd_builder = CommandBuilder("", "", DummyNER())#CommandBuilder("model.bin", "tags.txt")
+
+    def handle_ner_request(self, request: NERRequest):
+        result = self.cmd_builder.get_task(request.sentence)
+        task = Task(type="take")
+        return NERResponse(task=task)
+
+    def ner_server(self):
+        rospy.init_node("ner_server")
+        s = rospy.Service("ner", NER, self.handle_ner_request)
+        rospy.spin()
 
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    service = NERService()
+    service.ner_server()
--- ner/setup.py
+++ ner/setup.py
@@ -4,14 +4,13 @@ package_name = 'ner'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=["ner"],
     install_requires=['setuptools',
                         'torch',
                         'numpy'],
-    zip_safe=True,
     maintainer='',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=[],
+    tests_require=[]
 )
