5a4b0e76e1047c5a47e34232469830eca282b60a
aso <aso>
2021-03-02 15:15:44 +0100

    ROS commit that can build and run

--- grounding/nodes/grounding_node.py
+++ grounding/nodes/grounding_node.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 import rospy
-import msgs
+from std_msgs.msg import String
+from little_helper_interfaces.msg import ObjectEntity, ObjectInfo
 from grounding.grounding import Grounding
 
 class GroundingNode():
     def __init__(self):
-        self.entitypub = rospy.Publisher('GroundingEntity', msgs.ObjectEntity.msg, queue_size=10)
-        self.infopub = rospy.Publisher('GroundingInfo', msgs.ObjectInfo.msg, queue_size=10)
+        self.entitypub = rospy.Publisher('GroundingEntity', ObjectEntity, queue_size=10)
+        self.infopub = rospy.Publisher('GroundingInfo', ObjectInfo, queue_size=10)
         self.grounding = Grounding()
 
         while not rospy.is_shutdown():
             self.listener()
 
     def listener(self):
-        rospy.Subscriber('VisionData', int, self.grounding.vision_callback)  # TODO replace type
-        rospy.Subscriber('MainLearn', int, self.grounding.learn_new_object)  # TODO replace type
-        rospy.Subscriber('MainFind',  msgs.ObjectEntity.msg, self.find_object_callback)  # TODO replace type
+        rospy.Subscriber('VisionData', String, self.grounding.vision_callback)  # TODO replace type
+        rospy.Subscriber('MainLearn', String, self.grounding.learn_new_object)  # TODO replace type
+        rospy.Subscriber('MainFind',  ObjectEntity, self.find_object_callback)  # TODO replace type
         # Wait for messages on topic, go to callback function when new messages arrive.
         rospy.spin()
 
@@ -30,6 +31,7 @@ class GroundingNode():
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('grounding', anonymous=True)
         GroundingNode()
     except rospy.ROSInterruptException:
         pass
