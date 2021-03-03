458c4542c459aa7857cbe17f35516f2f79f26237
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-03-03 13:59:10 +0100

    Removed dummy implementation and added command line arguments for config

--- ner/nodes/ner_node.py
+++ ner/nodes/ner_node.py
@@ -5,57 +5,38 @@ from ner.command_builder import CommandBuilder, PickUpTask, FindTask
 from ner.ner import EntityType
 from little_helper_interfaces.msg import Task, ObjectEntity, OuterObjectEntity, SpatialDescription, OuterTask
 from ner.srv import NER, NERResponse, NERRequest
+import argparse
 
 
-class DummyNER:
-    def get_entities(self, sentence):
-        return [
-            (EntityType.FIND, "find"),
-            (EntityType.COLOUR, "blue"),
-            (EntityType.OBJECT, "bottom cover"),
-            (EntityType.LOCATION, "next"),
-            (EntityType.COLOUR, "black"),
-            (EntityType.OBJECT, "cover"),
-            (EntityType.LOCATION, "above"),
-            (EntityType.COLOUR, "yellow"),
-            (EntityType.OBJECT, "bottom cover"),
-            (EntityType.TAKE, "pick up"),
-            (EntityType.COLOUR, "red"),
-            (EntityType.OBJECT, "fuse"),
-            (EntityType.LOCATION, "next"),
-            (EntityType.COLOUR, "blue"),
-            (EntityType.OBJECT, "cover")
-        ]
+def create_ros_task(task):
+    ros_task = Task()
+    object1_entity = None
+    if isinstance(task, PickUpTask):
+        ros_task.type = "pick_up"
+        object1_entity = task.object_to_pick_up
+    elif isinstance(task, FindTask):
+        ros_task.type = "find"
+        object1_entity = task.object_to_find
+    ros_task.object1 = OuterObjectEntity(name=object1_entity.name, spatial_descriptions=[])
+    for spatial_description in object1_entity.spatial_descriptions:
+        ros_task.object1.spatial_descriptions.append(SpatialDescription(spatial_type=spatial_description.spatial_type.value,
+                                                                    object_entity=ObjectEntity(
+                                                                        name=spatial_description.object_entity.name)))
+    return ros_task
 
 
 class NERService:
-    def __init__(self):
-        self.cmd_builder = CommandBuilder("", "", DummyNER())#CommandBuilder("model.bin", "tags.txt")
-
-    def create_ros_task(self, task):
-        ros_task = Task()
-        object1_entity = None
-        if isinstance(task, PickUpTask):
-            ros_task.type = "pick_up"
-            object1_entity = task.object_to_pick_up
-        elif isinstance(task, FindTask):
-            ros_task.type = "find"
-            object1_entity = task.object_to_find
-        ros_task.object1 = OuterObjectEntity(name=object1_entity.name, spatial_descriptions=[])
-        for spatial_description in object1_entity.spatial_descriptions:
-            ros_task.object1.spatial_descriptions.append(SpatialDescription(spatial_type=spatial_description.spatial_type.value,
-                                                                        object_entity=ObjectEntity(
-                                                                            name=spatial_description.object_entity.name)))
-        return ros_task
+    def __init__(self, model_path, tags_path):
+        self.cmd_builder = CommandBuilder(model_path, tags_path)
 
     def handle_ner_request(self, request: NERRequest):
         task = self.cmd_builder.get_task(request.sentence)
-        ros_task = self.create_ros_task(task)
+        ros_task = create_ros_task(task)
         ros_outer_task = OuterTask(type=ros_task.type, object1=ros_task.object1, object2=ros_task.object2, child_tasks=[])
 
         if len(task.child_tasks) > 0:
             for child_task in task.child_tasks:
-                ros_task = self.create_ros_task(child_task)
+                ros_task = create_ros_task(child_task)
                 ros_outer_task.child_tasks.append(ros_task)
         return NERResponse(task=ros_outer_task)
 
@@ -66,6 +47,12 @@ class NERService:
 
 
 if __name__ == '__main__':
-    service = NERService()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", dest="model_path", default="model.bin", help="The path to the weight file for the model")
+    parser.add_argument("-t", "--tag_file", dest="tags_path", default="tags.txt",
+                        help="The path to the tags file")
+    print(rospy.myargv())
+    args = parser.parse_args(rospy.myargv()[1:])
+    service = NERService(args.model_path, args.tags_path)
     service.ner_server()
 
