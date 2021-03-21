8b7b8cd6785d2d4c669fae4b6b28e8240bdbe4e2
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-03-21 16:16:55 +0100

    Added camera interface in ur_e_webots package

--- ur_e_webots/src/ur_e_webots/gripper_action_server.py
+++ ur_e_webots/src/ur_e_webots/gripper_action_server.py
@@ -1,6 +1,9 @@
 import rospy
 import actionlib
-from ur_e_webots.msg import GripperAction
+import actionlib_msgs
+import numpy as np
+from cv_bridge import CvBridge
+from ur_e_webots.msg import GripperAction, GripperResult
 
 
 class GripperActionServer:
@@ -24,6 +27,10 @@ class GripperActionServer:
         self.suction = suction
         self.gripper_connector = gripper_connector
         self.gripper_connector_for_box = gripper_connector_for_box
+        self.camera_rgb = robot.getDevice("cameraRGB")
+        self.camera_depth = robot.getDevice("cameraDepth")
+        self.camera_enabled = False
+        self.cv_bridge = CvBridge()
         for sensor in self.finger_sensors:
             sensor.enable(self.timestep)
 
@@ -33,18 +40,19 @@ class GripperActionServer:
 
     def on_goal(self, goal_handle):
         self.goal_handle = goal_handle
+        goal_handle.set_accepted()
         if self.goal_handle.get_goal().action == "close":
             width = self.goal_handle.get_goal().width
             speed = self.goal_handle.get_goal().speed
             self.should_lock = self.goal_handle.get_goal().lock
             self.should_grip_box = self.goal_handle.get_goal().grip_box
-            if width > MAX_FINGER_DISTANCE:
-                rospy.logwarn(f"WARNING: Width over max distance of {MAX_FINGER_DISTANCE}mm. Clamping value")
-                width = MAX_FINGER_DISTANCE
+            if width > GripperActionServer.MAX_FINGER_DISTANCE:
+                rospy.logwarn(f"WARNING: Width over max distance of {GripperActionServer.MAX_FINGER_DISTANCE}mm. Clamping value")
+                width = GripperActionServer.MAX_FINGER_DISTANCE
             elif width < 0:
                 rospy.logwarn("WARNING: Width under min distance of 0mm. Clamping value")
                 width = 0
-            percent_closed = (MAX_FINGER_DISTANCE - width) / MAX_FINGER_DISTANCE
+            percent_closed = (GripperActionServer.MAX_FINGER_DISTANCE - width) / GripperActionServer.MAX_FINGER_DISTANCE
             right_finger_position = GripperActionServer.FINGER_OPEN_POSITION[0] - (GripperActionServer.FINGER_OPEN_POSITION[0] - GripperActionServer.FINGER_CLOSED_POSITION[0]) * percent_closed
             left_finger_position = GripperActionServer.FINGER_OPEN_POSITION[1] - (GripperActionServer.FINGER_OPEN_POSITION[1] - GripperActionServer.FINGER_CLOSED_POSITION[1]) * percent_closed
             self.finger_position = (left_finger_position, right_finger_position)
@@ -63,13 +71,18 @@ class GripperActionServer:
             self.suction.lock()
         elif self.goal_handle.get_goal().action == "suction_off":
             self.suction.unlock()
-        goal_handle.set_accepted()
+        elif self.goal_handle.get_goal().action == "get_image":
+            self.camera_rgb.enable(self.timestep)
+            self.camera_enabled = True
+        elif self.goal_handle.get_goal().action == "get_depth":
+            self.camera_depth.enable(self.timestep)
+            self.camera_enabled = True
 
     def on_cancel(self, goal_handle):
         if goal_handle == self.goal_handle:
             # stop the motors
             for index, motor in enumerate(self.finger_motors):
-                motor.setPosition(self.sensors[index].getValue())
+                motor.setPosition(self.finger_sensors[index].getValue())
             self.goal_handle.set_canceled()
             self.goal_handle = None
         else:
@@ -82,9 +95,9 @@ class GripperActionServer:
             if self.goal_handle.get_goal_status().status == actionlib_msgs.msg.GoalStatus.ACTIVE:
                 if action == "close":
                     if self.finger_sensors[0].getValue() - 0.0001 <= self.finger_position[1] <= self.finger_sensors[0].getValue() + 0.0001:
-                        if lock:
+                        if self.goal_handle.get_goal().lock:
                             self.gripper_connector.lock()
-                        if gripping_box:
+                        if self.goal_handle.get_goal().gripping_box:
                             self.gripper_connector_for_box.lock()
                         self.goal_handle.set_succeeded()
                 elif action == "open":
@@ -92,3 +105,21 @@ class GripperActionServer:
                         self.goal_handle.set_succeeded()
                 elif action == "suction_on" or action == "suction_off":
                     self.goal_handle.set_succeeded() # could maybe just be done in the on_goal
+                elif action == "get_image" and self.camera_enabled:
+                    np_img = np.array(self.camera_rgb.getImageArray(), dtype=np.uint8)
+                    np_img = np_img.transpose((1, 0, 2))
+                    np_img = np_img[:, :, ::-1]  # BGR ordering
+                    image_message = self.cv_bridge.cv2_to_compressed_imgmsg(np_img)
+                    response = GripperResult()
+                    response.success = True
+                    response.rgb_compressed = image_message
+                    self.goal_handle.set_succeeded(response)
+                    self.camera_enabled = False
+                elif action == "get_depth" and self.camera_enabled:
+                    np_img = np.array(self.camera_depth.getRangeImageArray())
+                    self.camera_enabled = False
+                    image_message = self.cv_bridge.cv2_to_compressed_imgmsg(np_img)
+                    response = GripperResult()
+                    response.success = True
+                    response.depth_compressed = image_message
+                    self.goal_handle.set_succeeded(response)
