3a487ec5a4d6d9409f9554af4a755900df42c166
Benedek <bgerga17@student.aau.dk>
2021-03-22 10:12:51 +0100

    changes to object finder
    changed crop width calculation to preserve pixel coordinates of an object, now returns both cropped and uncropped masks in the result

--- utility/find_objets/find_objects.py
+++ utility/find_objets/find_objects.py
@@ -3,45 +3,54 @@ import numpy as np
 
 class FindObjects:
 
-    def __init__(self, background_img=None, crop_region=None):
-        if crop_region is None:
-            crop_region = [0, 0, 1080, 1920]
-        self.crop_region = crop_region
+    def __init__(self, background_img=None, crop_widths=None):
+        """
+        :param background_img:
+        :param crop_widths: top, bottom, left, right
+        """
+        if crop_widths is None:
+            crop_widths = [0, 0, 0, 0]
+        self.crop_widths = crop_widths
         self.scale = 1.0
         self.diff_threshold = 20
         self.size_threshold = 500*self.scale
         self.background_img = background_img
 
-    def find_objects(self, image, crop_to_bbox=False):
+
+    def find_objects(self, image, debug=False):
         if self.background_img is None:
             raise Exception("background image must be set before running")
-        background_img = self.background_img[self.crop_region[0]:self.crop_region[2], self.crop_region[1]:self.crop_region[3]].copy()
+        background_img = self.background_img.copy()
+        size = background_img.shape
+        background_img = background_img[self.crop_widths[0]:size[0]-self.crop_widths[1], self.crop_widths[2]:size[1]-self.crop_widths[3]]
+        background_img = cv2.copyMakeBorder(background_img, self.crop_widths[0], self.crop_widths[1], self.crop_widths[2], self.crop_widths[3], cv2.BORDER_CONSTANT)
         background_img = cv2.resize(background_img, (int(background_img.shape[1] * self.scale), int(background_img.shape[0] * self.scale)))
         current_size = (background_img.shape[1], background_img.shape[0])
-        #cv2.imshow("background", background)
+        if debug: cv2.imshow("background", background_img)
         original_img = image.copy()
-        original_img = original_img[self.crop_region[0]:self.crop_region[2], self.crop_region[1]:self.crop_region[3]]
+        original_img = original_img[self.crop_widths[0]:size[0] - self.crop_widths[1], self.crop_widths[2]:size[1] - self.crop_widths[3]]
+        original_img = cv2.copyMakeBorder(original_img, self.crop_widths[0], self.crop_widths[1], self.crop_widths[2], self.crop_widths[3], cv2.BORDER_CONSTANT)
         original_img = cv2.resize(original_img, (int(original_img.shape[1] * self.scale), int(original_img.shape[0] * self.scale)))
-        #cv2.imshow("img", original_img)
+        if debug: cv2.imshow("img", original_img)
 
         diff = cv2.subtract(background_img, original_img)
 
         diff_g = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-        #cv2.imshow("diff", diff)
+        if debug: cv2.imshow("diff", diff)
         #cv2.imshow("diff_g", diff_g)
 
         _, thresholded_img = cv2.threshold(diff_g, self.diff_threshold, 255, cv2.THRESH_BINARY)
-        #cv2.imshow("thr", thresholded_img)
+        if debug: cv2.imshow("thr", thresholded_img)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
         thresholded_img = cv2.morphologyEx(thresholded_img, cv2.MORPH_CLOSE, kernel)
         thresholded_img = cv2.morphologyEx(thresholded_img, cv2.MORPH_OPEN, kernel)
-        #cv2.imshow("morph", thresholded_img)
+        if debug: cv2.imshow("morph", thresholded_img)
 
         contours, _ = cv2.findContours(thresholded_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         contours_img = cv2.cvtColor(thresholded_img, cv2.COLOR_GRAY2BGR)
         contours_img = cv2.drawContours(contours_img, contours, -1, (0,255,0), 1)
-        #cv2.imshow("cont", contours_img)
+        if debug: cv2.imshow("cont", contours_img)
 
         list_of_masked_images = []
         for i, contour in enumerate(contours):
@@ -53,17 +62,17 @@ class FindObjects:
             mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
             #cv2.imshow("mask", mask)
             masked_img = cv2.bitwise_and(original_img, original_img, mask=mask)
-            if crop_to_bbox:
-                x, y, w, h = cv2.boundingRect(contour)
-                masked_img = masked_img[y:y+h, x:x+w]
-            #cv2.imshow("masked"+str(i), masked_img)
-            list_of_masked_images.append(masked_img)
+            x, y, w, h = cv2.boundingRect(contour)
+            cropped_img = masked_img[y:y+h, x:x+w].copy()
+            if debug: cv2.imshow("masked"+str(i), masked_img)
+            list_of_masked_images.append([masked_img, cropped_img])
         return list_of_masked_images
 
 if __name__ == "__main__":
     background_img = cv2.imread("a.png")
     img = cv2.imread("b.png")
-    object_finder = FindObjects(img, [20, 180, 1050, 1350])
-    result = object_finder.find_objects(img, crop_to_bbox=False)
-    result2 = object_finder.find_objects(img, crop_to_bbox=True)
+    object_finder = FindObjects(background_img, crop_widths=[50, 50, 200, 600])
+    object_finder.scale = 0.4
+    result = object_finder.find_objects(img, debug=True)
+    #result2 = object_finder.find_objects(img, debug=True)
     cv2.waitKey()
