cb3e966ead7f0af4e2d27d69e53dfef673c3e244
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-04-08 14:40:30 +0200

    Use correct field for error checking

--- dialog_flow/nodes/dialog_flow.py
+++ dialog_flow/nodes/dialog_flow.py
@@ -99,7 +99,7 @@ class DialogFlow:
         np_depth = self.camera.get_depth()
 
         grounding_return = self.grounding.find_object(task.object_to_pick_up)
-        if grounding_return is None:
+        if grounding_return.object_info is None:
             sentence = "I could not find the object you were looking for. Maybe I need some calibration."
             self.tts(sentence)
             rospy.loginfo(sentence)
