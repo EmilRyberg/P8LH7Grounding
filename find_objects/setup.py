9d6dbfe1fa7fdee4dfdbdf468729a9e8806f42bf
Emil Albin Ryberg <eryber17@student.aau.dk>
2021-05-05 15:47:41 +0200

    Fixed name in setup.py

--- find_objects/setup.py
+++ find_objects/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-package_name = 'find_objects'
+package_name = 'find_objects_lib'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=["find_objects"],
+    packages=["find_objects_lib"],
     install_requires=['setuptools',
                         'numpy'],
     maintainer='',
